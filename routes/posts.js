d644b6f82907bb6a9bdf9f0e1d6a601e3dbb6e88
reader00 <qoryamanahputra@gmail.com>
2024-05-06 07:32:32 +0700

    cpuram 5

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getFeedPosts, getUserPosts, likePost } from '../controllers/posts.js';
+import { getFeeds, getUserPosts, likePost } from '../controllers/posts.js';
 import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
 /* READ */
-router.get('/', verifyToken, getFeedPosts);
+router.get('/', verifyToken, getFeeds);
 router.get('/:id', verifyToken, getUserPosts);
 
 /* UPDATE */
